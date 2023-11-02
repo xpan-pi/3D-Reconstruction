3719e482363cd85c5e2f1f13711fa952635a3f78
Ruan-xianzheng <106811961+Ruan-xianzheng@users.noreply.github.com>
2023-11-02 19:00:58 +0800

    semantic_segmentation.py

---
+++ semantic_segmentation.py
@@ -0,0 +1,382 @@
+#----------------------------------------------------#
+#  用于工件语义分割
+#----------------------------------------------------#
+import colorsys
+import copy
+import time
+import cv2
+import numpy as np
+import torch.nn.functional as F
+import torch
+from PIL import Image
+from torch import nn
+
+# from unet import Unet
+from nets.unet import Unet as unet1
+from utils.utils import cvtColor, preprocess_input, resize_image
+#--------------------------------------------#
+#   使用自己训练好的模型预测需要修改2个参数
+#   model_path和num_classes都需要修改！
+#   如果出现shape不匹配
+#   一定要注意训练时的model_path和num_classes数的修改
+#--------------------------------------------#
+class Unet(object):
+    #-------------------------------------------------------------------------#
+    #   如果想要修改对应种类的颜色，到__init__函数里修改self.colors即可
+    #-------------------------------------------------------------------------#
+    #----------------------------------------------------------------------------------------------------------#
+    #   mode用于指定测试的模式：
+    #   'predict'           表示单张图片预测，如果想对预测过程进行修改，如保存图片，截取对象等，可以先看下方详细的注释
+    #   'fps'               表示测试fps，使用的图片是img里面的street.jpg，详情查看下方注释。
+    #   'dir_predict'       表示遍历文件夹进行检测并保存。默认遍历img文件夹，保存img_out文件夹，详情查看下方注释。
+    #----------------------------------------------------------------------------------------------------------#
+    mode = "dir_predict"
+    #-------------------------------------------------------------------------#
+    #   count               指定了是否进行目标的像素点计数（即面积）与比例计算
+    #   name_classes        区分的种类，和json_to_dataset里面的一样，用于打印种类和数量
+    #   count、name_classes仅在mode='predict'时有效
+    #-------------------------------------------------------------------------#
+    count           = True
+    name_classes    = ["background","Big_model"]
+    #----------------------------------------------------------------------------------------------------------#
+    #   test_interval       用于指定测量fps的时候，图片检测的次数。理论上test_interval越大，fps越准确。
+    #   fps_image_path      用于指定测试的fps图片
+    #   
+    #   test_interval和fps_image_path仅在mode='fps'有效
+    #----------------------------------------------------------------------------------------------------------#
+    test_interval = 100
+    fps_image_path  = "img/street.jpg"
+    #-------------------------------------------------------------------------#
+    #   dir_origin_path     指定了用于检测的图片的文件夹路径
+    #   dir_save_path       指定了检测完图片的保存路径
+    #   
+    #   dir_origin_path和dir_save_path仅在mode='dir_predict'时有效
+    #-------------------------------------------------------------------------#
+    dir_origin_path = "imgs/"
+    dir_save_path   = "semantic_imgs_out/"
+    
+    #-------------------------------------------------------------------#
+    #   model_path指向logs文件夹下的权值文件
+    #   训练好后logs文件夹下存在多个权值文件，选择验证集损失较低的即可。
+    #   验证集损失较低不代表miou较高，仅代表该权值在验证集上泛化性能较好。
+    #-------------------------------------------------------------------#
+    model_path    = 'tools/best_epoch_weights.pth'
+    #--------------------------------#
+    #   所需要区分的类的个数+1
+    #--------------------------------#
+    num_classes   = 2
+    #--------------------------------#
+    #   所使用的的主干网络：vgg、resnet50   
+    #--------------------------------#
+    backbone      = "vgg"
+    #--------------------------------#
+    #   输入图片的大小
+    #--------------------------------#
+    input_shape   = [2688, 1520]
+    #-------------------------------------------------#
+    #   mix_type参数用于控制检测结果的可视化方式
+    #   mix_type = 0的时候代表原图与生成的图进行混合
+    #   mix_type = 1的时候代表仅保留生成的图
+    #   mix_type = 2的时候代表仅扣去背景，仅保留原图中的目标
+    #-------------------------------------------------#
+    mix_type      = 1
+    #--------------------------------#
+    #   是否使用Cuda
+    #   没有GPU可以设置成False
+    #--------------------------------#
+    cuda          = True
+
+    #---------------------------------------------------#
+    #   初始化UNET
+    #---------------------------------------------------#
+    def __init__(self, **kwargs):
+        # self.__dict__.update(self._defaults)
+        for name, value in kwargs.items():
+            setattr(self, name, value)
+        #---------------------------------------------------#
+        #   画框设置不同的颜色
+        #---------------------------------------------------#
+        if self.num_classes <= 21:
+            self.colors = [ (0, 0, 0), (128, 0, 0), (0, 128, 0), (128, 128, 0), (0, 0, 128), (128, 0, 128), (0, 128, 128), 
+                            (128, 128, 128), (64, 0, 0), (192, 0, 0), (64, 128, 0), (192, 128, 0), (64, 0, 128), (192, 0, 128), 
+                            (64, 128, 128), (192, 128, 128), (0, 64, 0), (128, 64, 0), (0, 192, 0), (128, 192, 0), (0, 64, 128), 
+                            (128, 64, 12)]
+        else:
+            hsv_tuples = [(x / self.num_classes, 1., 1.) for x in range(self.num_classes)]
+            self.colors = list(map(lambda x: colorsys.hsv_to_rgb(*x), hsv_tuples))
+            self.colors = list(map(lambda x: (int(x[0] * 255), int(x[1] * 255), int(x[2] * 255)), self.colors))
+        #---------------------------------------------------#
+        #   获得模型
+        #---------------------------------------------------#
+        self.generate()
+        
+        # show_config(**self._defaults)
+
+    #---------------------------------------------------#
+    #   获得所有的分类
+    #---------------------------------------------------#
+    def generate(self, onnx=False):
+        self.net = unet1(num_classes = self.num_classes, backbone=self.backbone)
+
+        device      = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        self.net.load_state_dict(torch.load(self.model_path, map_location=device))
+        self.net    = self.net.eval()
+        print('{} model, and classes loaded.'.format(self.model_path))
+        if not onnx:
+            if self.cuda:
+                self.net = nn.DataParallel(self.net)
+                self.net = self.net.cuda()
+
+    #---------------------------------------------------#
+    #   检测图片
+    #---------------------------------------------------#
+    def detect_image(self, image, count=False, name_classes=None):
+        #---------------------------------------------------------#
+        #   在这里将图像转换成RGB图像，防止灰度图在预测时报错。
+        #   代码仅仅支持RGB图像的预测，所有其它类型的图像都会转化成RGB
+        #---------------------------------------------------------#
+        image       = cvtColor(image)
+        #---------------------------------------------------#
+        #   对输入图像进行一个备份，后面用于绘图
+        #---------------------------------------------------#
+        old_img     = copy.deepcopy(image)
+        orininal_h  = np.array(image).shape[0]
+        orininal_w  = np.array(image).shape[1]
+        #---------------------------------------------------------#
+        #   给图像增加灰条，实现不失真的resize
+        #   也可以直接resize进行识别
+        #---------------------------------------------------------#
+        image_data, nw, nh  = resize_image(image, (self.input_shape[1],self.input_shape[0]))
+        #---------------------------------------------------------#
+        #   添加上batch_size维度
+        #---------------------------------------------------------#
+        image_data  = np.expand_dims(np.transpose(preprocess_input(np.array(image_data, np.float32)), (2, 0, 1)), 0)
+
+        with torch.no_grad():
+            images = torch.from_numpy(image_data)
+            if self.cuda:
+                images = images.cuda()
+            #---------------------------------------------------#
+            #   图片传入网络进行预测
+            #---------------------------------------------------#
+            pr = self.net(images)[0]
+            #---------------------------------------------------#
+            #   取出每一个像素点的种类
+            #---------------------------------------------------#
+            pr = F.softmax(pr.permute(1,2,0),dim = -1).cpu().numpy()
+            #--------------------------------------#
+            #   将灰条部分截取掉
+            #--------------------------------------#
+            pr = pr[int((self.input_shape[0] - nh) // 2) : int((self.input_shape[0] - nh) // 2 + nh), \
+                    int((self.input_shape[1] - nw) // 2) : int((self.input_shape[1] - nw) // 2 + nw)]
+            #---------------------------------------------------#
+            #   进行图片的resize
+            #---------------------------------------------------#
+            pr = cv2.resize(pr, (orininal_w, orininal_h), interpolation = cv2.INTER_LINEAR)
+            #---------------------------------------------------#
+            #   取出每一个像素点的种类
+            #---------------------------------------------------#
+            pr = pr.argmax(axis=-1)
+        #---------------------------------------------------------#
+        #   计数
+        #---------------------------------------------------------#
+        if count:
+            classes_nums        = np.zeros([self.num_classes])
+            total_points_num    = orininal_h * orininal_w
+            print('-' * 63)
+            print("|%25s | %15s | %15s|"%("Key", "Value", "Ratio"))
+            print('-' * 63)
+            for i in range(self.num_classes):
+                num     = np.sum(pr == i)
+                ratio   = num / total_points_num * 100
+                if num > 0:
+                    print("|%25s | %15s | %14.2f%%|"%(str(name_classes[i]), str(num), ratio))
+                    print('-' * 63)
+                classes_nums[i] = num
+            print("classes_nums:", classes_nums)
+
+        if self.mix_type == 0:
+            seg_img = np.reshape(np.array(self.colors, np.uint8)[np.reshape(pr, [-1])], [orininal_h, orininal_w, -1])
+            #------------------------------------------------#
+            #   将新图片转换成Image的形式
+            #------------------------------------------------#
+            image   = Image.fromarray(np.uint8(seg_img))
+            #------------------------------------------------#
+            #   将新图与原图及进行混合
+            #------------------------------------------------#
+            image   = Image.blend(old_img, image, 0.7)
+
+        elif self.mix_type == 1:
+            seg_img = np.reshape(np.array(self.colors, np.uint8)[np.reshape(pr, [-1])], [orininal_h, orininal_w, -1])
+            #------------------------------------------------#
+            #   将新图片转换成Image的形式
+            #------------------------------------------------#
+            image   = Image.fromarray(np.uint8(seg_img))
+
+        elif self.mix_type == 2:
+            seg_img = (np.expand_dims(pr != 0, -1) * np.array(old_img, np.float32)).astype('uint8')
+            #------------------------------------------------#
+            #   将新图片转换成Image的形式
+            #------------------------------------------------#
+            image = Image.fromarray(np.uint8(seg_img))
+        
+        return image
+
+    def get_FPS(self, image, test_interval):
+        #---------------------------------------------------------#
+        #   在这里将图像转换成RGB图像，防止灰度图在预测时报错。
+        #   代码仅仅支持RGB图像的预测，所有其它类型的图像都会转化成RGB
+        #---------------------------------------------------------#
+        image       = cvtColor(image)
+        #---------------------------------------------------------#
+        #   给图像增加灰条，实现不失真的resize
+        #   也可以直接resize进行识别
+        #---------------------------------------------------------#
+        image_data, nw, nh  = resize_image(image, (self.input_shape[1],self.input_shape[0]))
+        #---------------------------------------------------------#
+        #   添加上batch_size维度
+        #---------------------------------------------------------#
+        image_data  = np.expand_dims(np.transpose(preprocess_input(np.array(image_data, np.float32)), (2, 0, 1)), 0)
+
+        with torch.no_grad():
+            images = torch.from_numpy(image_data)
+            if self.cuda:
+                images = images.cuda()               
+            #---------------------------------------------------#
+            #   图片传入网络进行预测
+            #---------------------------------------------------#
+            pr = self.net(images)[0]
+            #---------------------------------------------------#
+            #   取出每一个像素点的种类
+            #---------------------------------------------------#
+            pr = F.softmax(pr.permute(1,2,0),dim = -1).cpu().numpy().argmax(axis=-1)
+            #--------------------------------------#
+            #   将灰条部分截取掉
+            #--------------------------------------#
+            pr = pr[int((self.input_shape[0] - nh) // 2) : int((self.input_shape[0] - nh) // 2 + nh), \
+                    int((self.input_shape[1] - nw) // 2) : int((self.input_shape[1] - nw) // 2 + nw)]
+
+        t1 = time.time()
+        for _ in range(test_interval):
+            with torch.no_grad():
+                #---------------------------------------------------#
+                #   图片传入网络进行预测
+                #---------------------------------------------------#
+                pr = self.net(images)[0]
+                #---------------------------------------------------#
+                #   取出每一个像素点的种类
+                #---------------------------------------------------#
+                pr = F.softmax(pr.permute(1,2,0),dim = -1).cpu().numpy().argmax(axis=-1)
+                #--------------------------------------#
+                #   将灰条部分截取掉
+                #--------------------------------------#
+                pr = pr[int((self.input_shape[0] - nh) // 2) : int((self.input_shape[0] - nh) // 2 + nh), \
+                        int((self.input_shape[1] - nw) // 2) : int((self.input_shape[1] - nw) // 2 + nw)]
+        t2 = time.time()
+        tact_time = (t2 - t1) / test_interval
+        return tact_time
+
+    def get_miou_png(self, image):
+        #---------------------------------------------------------#
+        #   在这里将图像转换成RGB图像，防止灰度图在预测时报错。
+        #   代码仅仅支持RGB图像的预测，所有其它类型的图像都会转化成RGB
+        #---------------------------------------------------------#
+        image       = cvtColor(image)
+        orininal_h  = np.array(image).shape[0]
+        orininal_w  = np.array(image).shape[1]
+        #---------------------------------------------------------#
+        #   给图像增加灰条，实现不失真的resize
+        #   也可以直接resize进行识别
+        #---------------------------------------------------------#
+        image_data, nw, nh  = resize_image(image, (self.input_shape[1],self.input_shape[0]))
+        #---------------------------------------------------------#
+        #   添加上batch_size维度
+        #---------------------------------------------------------#
+        image_data  = np.expand_dims(np.transpose(preprocess_input(np.array(image_data, np.float32)), (2, 0, 1)), 0)
+
+        with torch.no_grad():
+            images = torch.from_numpy(image_data)
+            if self.cuda:
+                images = images.cuda()
+                
+            #---------------------------------------------------#
+            #   图片传入网络进行预测
+            #---------------------------------------------------#
+            pr = self.net(images)[0]
+            #---------------------------------------------------#
+            #   取出每一个像素点的种类
+            #---------------------------------------------------#
+            pr = F.softmax(pr.permute(1,2,0),dim = -1).cpu().numpy()
+            #--------------------------------------#
+            #   将灰条部分截取掉
+            #--------------------------------------#
+            pr = pr[int((self.input_shape[0] - nh) // 2) : int((self.input_shape[0] - nh) // 2 + nh), \
+                    int((self.input_shape[1] - nw) // 2) : int((self.input_shape[1] - nw) // 2 + nw)]
+            #---------------------------------------------------#
+            #   进行图片的resize
+            #---------------------------------------------------#
+            pr = cv2.resize(pr, (orininal_w, orininal_h), interpolation = cv2.INTER_LINEAR)
+            #---------------------------------------------------#
+            #   取出每一个像素点的种类
+            #---------------------------------------------------#
+            pr = pr.argmax(axis=-1)
+    
+        image = Image.fromarray(np.uint8(pr))
+        return image
+
+def main():
+    unet = Unet()  #unet初始化
+    if unet.mode == "predict":
+        '''
+        predict.py有几个注意点
+        1、该代码无法直接进行批量预测，如果想要批量预测，可以利用os.listdir()遍历文件夹，利用Image.open打开图片文件进行预测。
+        具体流程可以参考get_miou_prediction.py，在get_miou_prediction.py即实现了遍历。
+        2、如果想要保存，利用r_image.save("img.jpg")即可保存。
+        3、如果想要原图和分割图不混合，可以把blend参数设置成False。
+        4、如果想根据mask获取对应的区域，可以参考detect_image函数中，利用预测结果绘图的部分，判断每一个像素点的种类，然后根据种类获取对应的部分。
+        seg_img = np.zeros((np.shape(pr)[0],np.shape(pr)[1],3))
+        for c in range(self.num_classes):
+            seg_img[:, :, 0] += ((pr == c)*( self.colors[c][0] )).astype('uint8')
+            seg_img[:, :, 1] += ((pr == c)*( self.colors[c][1] )).astype('uint8')
+            seg_img[:, :, 2] += ((pr == c)*( self.colors[c][2] )).astype('uint8')
+        '''
+        while True:
+            img = input('Input image filename:')
+            try:
+                image = Image.open(img)
+            except:
+                print('Open Error! Try again!')
+                continue
+            else:
+                r_image = unet.detect_image(image, count=unet.count, name_classes=unet.name_classes)
+                r_image.show()
+
+    elif unet.mode == "fps":
+        img = Image.open('img/street.jpg')
+        tact_time = unet.get_FPS(img, unet.test_interval)
+        print(str(tact_time) + ' seconds, ' + str(1/tact_time) + 'FPS, @batch_size 1')
+        
+    elif unet.mode == "dir_predict":
+        import os
+        from tqdm import tqdm
+
+        img_names = os.listdir(unet.dir_origin_path)
+        for file in img_names:
+            if file.endswith('_r.jpg') or file.endswith('_l.jpg'):
+                image_path  = os.path.join(unet.dir_origin_path, file)
+                image       = Image.open(image_path)
+                r_image     = unet.detect_image(image)
+                # r_image = r_image.astype(float)
+                if not os.path.exists(unet.dir_save_path):
+                    os.makedirs(unet.dir_save_path)
+                r_image.save(os.path.join(unet.dir_save_path, file))
+                r_image.show()
+                # iamge_arr = np.array(r_image)
+                # cv2.imshow(file, iamge_arr,)
+                # cv2.waitKey(0)
+                # cv2.destroyAllWindows()           
+    else:
+        raise AssertionError("Please specify the correct mode: 'predict', 'video', 'fps' or 'dir_predict'.")
+
+if __name__ == "__main__":
+    main()
